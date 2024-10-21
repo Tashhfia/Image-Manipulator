99471fb9e3ea5f6783d856a1ae5e80fd8dfa336a
tashfia <hridishamim128@gmail.com>
2024-10-21 23:08:28 +0200

    flip

--- Skimage/manipulator.py
+++ Skimage/manipulator.py
@@ -10,10 +10,28 @@ class ImageManipulator():
     def showOriginalImage(self):
         plt.imshow(self.image)
         plt.show()
+    
+    def flip(self, axis):
+        if axis == 0:    # flip horizontally, flip rows
+            flipped_im = self.image[:, ::-1, :]
+        elif axis == 1:     # flip vertically, flip columns
+            flipped_im = self.image[::-1, :, :]
+        else:
+            print("Invalid axis value!")
+            flipped_im = self.image
+
+        return flipped_im
+    
+
 
 def main():
-    im = ImageManipulator("https://www.fau.de/files/2019/07/Kollegienhaus_Malter_3-480x284.jpg")
-    im.showOriginalImage()
+    im = ImageManipulator("https://i.pinimg.com/736x/8c/dc/a5/8cdca5e3979427241ae50537c9be932b.jpg")
+    k = im.flip(0)
+    plt.imshow(k)
+    plt.show()
+
+    #im.showOriginalImage()
+
 
 
 if __name__ == "__main__":

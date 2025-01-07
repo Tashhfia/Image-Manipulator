f321e36d72b17088a6bdf17ff235f7d0fa46cfee
tashfia <hridishamim128@gmail.com>
2025-01-07 01:51:28 +0100

    documentation update

--- Skimage/preprocess.py
+++ Skimage/preprocess.py
@@ -12,6 +12,7 @@ class Preprocess():
         plt.show()
     
     def flip(self, axis):
+        """ Takes in the image and the axis as arguements"""
         if axis == 0:    # flip horizontally, flip rows
             flipped_im = self.image[:, ::-1, :]
         elif axis == 1:     # flip vertically, flip columns
@@ -22,6 +23,16 @@ class Preprocess():
         return flipped_im
     
     def crop(self, x, y, width, height):
+        """ Crops the image to a specified rectangular region.
+
+        Parameters:
+        x (int): The x-coordinate (column) of the top-left corner of the crop region.
+        y (int): The y-coordinate (row) of the top-left corner of the crop region.
+        width (int): The width of the crop region.
+        height (int): The height of the crop region.
+
+        Returns: The cropped portion of the image as a NumPy array."""
+
         im_w = self.image.shape[1]  # columns dictate the width so we take the second element of shape
         im_h = self.image.shape[0]  # rows dictate the height so we take the first element of shape
         print(f"width is {im_w}")
@@ -39,8 +50,7 @@ class Preprocess():
         # First value (before the first comma): Controls height (rows, y).
         # Second value (after the first comma): Controls width (columns, x).
         cropped = self.image[y:(y + crop_height), x: (x + crop_width), :]
-        plt.imshow(cropped)
-        plt.show()
+        return cropped
 
 
     
@@ -48,10 +58,7 @@ class Preprocess():
 
 def main():
     im = Preprocess("https://i.pinimg.com/736x/8c/dc/a5/8cdca5e3979427241ae50537c9be932b.jpg")
-    im.crop(x=1000, y=150, width=128, height=128)
-    # k = im.flip(0)
-    # plt.imshow(k)
-    # plt.show()
+
 
     #im.showOriginalImage()
 

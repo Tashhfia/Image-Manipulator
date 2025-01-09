719f3334f457221f0cd1bcc8ed5d49ed6031abb0
tashfia <hridishamim128@gmail.com>
2025-01-09 01:41:27 +0100

    resize method added

--- Skimage/preprocess.py
+++ Skimage/preprocess.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 from skimage import io
+from skimage.transform import rescale, resize
 
 class Preprocess():
     def __init__(self, url) -> None:
         self.image = io.imread(url)
 
-    def showOriginalImage(self):
+    def show_im(self, im):
         plt.imshow(self.image)
         plt.show()
     
-    def flip(self, axis):
+    def flip_im(self, axis):
         """ Takes in the image and the axis as arguements"""
         if axis == 0:    # flip horizontally, flip rows
             flipped_im = self.image[:, ::-1, :]
@@ -22,7 +23,7 @@ class Preprocess():
             flipped_im = self.image
         return flipped_im
     
-    def crop(self, x, y, width, height):
+    def crop_im(self, x, y, width, height):
         """ Crops the image to a specified rectangular region.
 
         Parameters:
@@ -52,12 +53,30 @@ class Preprocess():
         cropped = self.image[y:(y + crop_height), x: (x + crop_width), :]
         return cropped
 
+    def resize_im(self, size):
+        if isinstance(size, tuple):
+            # make sure we get h and w
+            if len(size) != 2:
+                raise ValueError("Size tuple must contain exactly 2 elements (height, width).")
+            # make sure the value is correct for h and w
+            if not all(isinstance(dim, int) and dim > 0 for dim in size):
+                raise ValueError("Both dimensions in the tuple must be positive integers.")
 
+            return resize(self.image, size, anti_aliasing = True)
+
+        elif isinstance(size, float):
+            return rescale(self.image, size, anti_aliasing = True, channel_axis = 2)
+        else:
+            raise ValueError(f"{size} is an invalid dimension!")
     
 
 
 def main():
     im = Preprocess("https://i.pinimg.com/736x/8c/dc/a5/8cdca5e3979427241ae50537c9be932b.jpg")
+    k = im.resize_im(.4)
+    plt.imshow(k)
+    plt.show()
+
 
 
     #im.showOriginalImage()
